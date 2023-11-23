4532ce0ba9c22fd77f143e4b21b916ae4e8d9ba8
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-23 14:26:45 +0100

    [ add ] slug unique

--- database/migrations/2023_11_23_131138_create_comics_table.php
+++ database/migrations/2023_11_23_131138_create_comics_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
+            $table->string('slug', 100)->unique();
             $table->text('description')->nullable();
             $table->string('thumb');
             $table->string('price', 10);
