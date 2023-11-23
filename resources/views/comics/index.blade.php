bc1cd26cb95a9eaef49f251aa03bbba99183cc98
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-23 15:37:38 +0100

    [ init ] index comics with table products

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1>Lista fumetti</h1>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome Fumetto</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $comic)
+                    <tr>
+                        <td>{{ $comic->id }}</td>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td> <a href="{{ route('comics.show', $comic) }}" class="btn btn-success"> Dettagli</a> </td>
+
+                    </tr>
+                @endforeach
+
+
+            </tbody>
+        </table>
+    </div>
+@endsection
