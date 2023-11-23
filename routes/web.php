0d1a744de7a3e8e164f6945beee229d051d413dd
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-23 15:16:51 +0100

    [ add ] route pagecontroller and comics controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::resource('comics', ComicController::class);
