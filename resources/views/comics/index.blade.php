254c2e00db99cff06a613fd208b559c98c1bdab7
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-23 16:30:29 +0100

    [ change ] del container and add table details

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,33 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container">
-        <h1>Lista fumetti</h1>
+    <h1>Lista fumetti</h1>
 
-        <table class="table">
-            <thead>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome Fumetto</th>
+                <th scope="col">Series</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $comic)
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome Fumetto</th>
-                    <th scope="col">Series</th>
-                    <th scope="col">Prezzo</th>
-                    <th scope="col">Azioni</th>
+                    <td>{{ $comic->id }}</td>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td> <a href="{{ route('comics.show', $comic) }}" class="btn btn-success"> Dettagli</a> </td>
+
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $comic)
-                    <tr>
-                        <td>{{ $comic->id }}</td>
-                        <td>{{ $comic->title }}</td>
-                        <td>{{ $comic->series }}</td>
-                        <td>{{ $comic->price }}</td>
-                        <td> <a href="{{ route('comics.show', $comic) }}" class="btn btn-success"> Dettagli</a> </td>
-
-                    </tr>
-                @endforeach
+            @endforeach
 
 
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
 @endsection
