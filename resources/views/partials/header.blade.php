ba2bcc2dfee9ea587b34fcd44cc59cb900ea1b46
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-23 15:38:15 +0100

    [ add ] route comics.index

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
                         <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">List Comics</a>
+                        <a class="nav-link" href="{{ route('comics.index') }}">List Comics</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">New Comic</a>
