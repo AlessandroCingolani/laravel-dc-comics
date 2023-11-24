e53b20e27d58db2996e8edc5a89e906ff7763dc6
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-24 15:01:47 +0100

    [ change ] formatting

--- resources/views/partials/formDelete.blade.php
+++ resources/views/partials/formDelete.blade.php
@@ -3,7 +3,7 @@
     @csrf
     @method('DELETE')
 
-    <button type="submit"class="btn btn-danger">
+    <button type="submit" class="btn btn-danger">
         <i class="fa-regular fa-trash-can"></i>
     </button>
 </form>
