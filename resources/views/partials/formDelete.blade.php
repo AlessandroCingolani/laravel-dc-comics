bdf7b7035031b37ec80422dfe3c6e726e508eec9
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-24 14:48:05 +0100

    [ init ] partials delete btn form

---
+++ resources/views/partials/formDelete.blade.php
@@ -0,0 +1,7 @@
+<form class="d-inline-block" action={{ route('comics.destroy', $comic) }} method="POST"
+    onsubmit="return confirm('Sicuro di eliminare {{ $comic->title }}?')" @csrf @method('DELETE') <button type="submit"
+    class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
+
+
+
+</form>
