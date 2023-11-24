9231fb834652982a31fcd60c901c0d63229a330b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-24 14:51:28 +0100

    [ fix ] formatting form

--- resources/views/partials/formDelete.blade.php
+++ resources/views/partials/formDelete.blade.php
@@ -1,7 +1,9 @@
 <form class="d-inline-block" action={{ route('comics.destroy', $comic) }} method="POST"
-    onsubmit="return confirm('Sicuro di eliminare {{ $comic->title }}?')" @csrf @method('DELETE') <button type="submit"
-    class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></button>
+    onsubmit="return confirm('Sicuro di eliminare {{ $comic->title }}?')">
+    @csrf
+    @method('DELETE')
 
-
-
+    <button type="submit"class="btn btn-danger">
+        <i class="fa-regular fa-trash-can"></i>
+    </button>
 </form>
